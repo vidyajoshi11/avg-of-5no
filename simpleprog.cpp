a916b502723fb347374c5f269deb09c73f5da3ba
vidyajoshi11 <148187787+vidyajoshi11@users.noreply.github.com>
2023-10-17 15:34:53 +0530

    Create simpleprog.cpp

---
+++ simpleprog.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+double n1,n2,n3,n4,n5;
+double avg;
+cout<<"enter five number";
+cin>>n1>>n2>>n3>>n4>>n5;
+avg =(n1+n2+n3+n4+n5)/5;
+cout<<"AVG="<<avg;
+return 0;
+}
