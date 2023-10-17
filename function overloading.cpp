b01ab3503db3fd355649af6f79481338079c52b8
vidyajoshi11 <148187787+vidyajoshi11@users.noreply.github.com>
2023-10-17 16:15:41 +0530

    Update function overloading.cpp

--- function overloading.cpp
+++ function overloading.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
-void add(int a, int b)
+void add(int a, int b,int c, int d, int e)
 {
-cout << "sum = " << (a + b);
+cout << "sum = " << (a + b + c + d + e);
 }
 
 void add(double a, double b)
@@ -12,7 +12,7 @@ void add(double a, double b)
 
 int main()
 {
-	add(10, 2);
+	add(10, 2, 5, 6, 3);
 	add(5.3, 6.2);
 
 	return 0;
